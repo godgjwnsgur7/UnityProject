afd94f761cc3f7b42144e568c23b426a69b94dfc
Jun <godgjwnsgur7@naver.com>
2025-01-21 17:56:05 +0900

    Add Zenject (Extenject)

--- JunStudyProject/Assets/@Scenes/TestScene/UI_TestScene.cs
+++ JunStudyProject/Assets/@Scenes/TestScene/UI_TestScene.cs
@@ -1,13 +1,22 @@
 using Cysharp.Threading.Tasks;
 using System;
+using UniRx;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_TestScene : MonoBehaviour
 {
     [SerializeField] TestScene scene;
 
+    [SerializeField] Button btn;
+
     public event Func<bool, bool> IsTestCheck;
-    
+
+    private void Awake()
+    {
+        btn.OnClickAsObservable().Subscribe(_ => OnClickTest1());
+    }
+
     public void OnClickTest1()
     {
         bool b = IsTestCheck.Invoke(true);
